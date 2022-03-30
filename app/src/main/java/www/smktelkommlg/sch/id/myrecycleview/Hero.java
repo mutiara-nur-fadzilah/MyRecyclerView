0e7bc7aac5f33740ef740402a770481150b8bb7b
mutiara-nur-fadzilah <mutiara_nur_29rpl@student.smktelkom-mlg.sch.id>
2022-03-30 11:40:10 +0700

    Initial commit

---
+++ app/src/main/java/www/smktelkommlg/sch/id/myrecycleview/Hero.java
@@ -0,0 +1,31 @@
+package www.smktelkommlg.sch.id.myrecycleview;
+
+public class Hero {
+    private String name;
+    private String detail;
+    private int photo;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    public int getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(int photo) {
+        this.photo = photo;
+    }
+}
